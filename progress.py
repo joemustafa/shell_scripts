7f478c58f2827ddb0bcf8d3158142426cb6a80ed
Joe Mustafa <jmustafa@gmail.com>
2020-12-13 21:21:55 -0600

    Create progress.py

---
+++ progress.py
@@ -0,0 +1,8 @@
+def _progress_bar(self, count, status=''):
+  bar_len = 50
+  total = 100
+  filled_len = int(round(bar_len * count / float(total)))
+  percents = round(100.0 * count / float(total), 1)
+  bar = '*' * filled_len + '-' * (bar_len - filled_len)
+  sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+  sys.stdout.flush()
